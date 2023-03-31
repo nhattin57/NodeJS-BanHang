2025ec4654d50d7601f971f514c80c9be0d37e8f
Đào Nhật Tín <nhattin57@gmail.com>
2023-03-31 13:44:29 +0700

    login, register, authen, author

--- controller/member.js
+++ controller/member.js
@@ -1,4 +1,71 @@
 import { memberRepository } from "../repositories/index.js"
+import {EventEmitter} from 'node:events'
+
+const myEvent = new EventEmitter()
+//listen
+myEvent.on('event.register.member',(params) =>{
+    console.log(`they talked about: ${JSON.stringify(params)}`)
+})
+
+const login = async (req, res) =>{
+
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //     return res.status(400).json({ 
+    //   errors: errors.array() 
+    // });
+
+    // }
+    const {email, password} = req.body;
+    //call repository
+    try {
+       let existingMember = await memberRepository.login({email, password})
+        res.status(200).json({
+        message: 'Login Member successfully',
+        data : existingMember
+    })
+    } catch (exception) {
+      res.status(500).json({
+        message: exception.toString()
+    })
+    }
+}
+
+
+
+const register = async (req, res) =>{
+    //destructuring
+  const {
+    name,
+    email, 
+    password,
+    phone
+  } = req.body
+
+    
+    //event + emitter
+    myEvent.emit('event.register.member', {email, password})
+
+    try {
+        debugger
+       const member = await memberRepository.register({
+            email,
+            password, 
+            name, 
+            phone
+       })
+       res.status(201).json({
+        message: 'register member successfully',
+        data : member
+    })
+    } catch (exception) {
+        debugger
+        res.status(500).json({
+            message: exception.toString()
+        })
+    }
+
+}
 
 async function getAllMember(req, res){
     
@@ -17,5 +84,7 @@ async function getAllMember(req, res){
 }
 
 export default {
-    getAllMember
+    getAllMember,
+    register,
+    login
 }
--- repositories/product.js
+++ repositories/product.js
@@ -1,11 +1,40 @@
 import Exception from "../Exceptions/Exception.js"
 import { Product } from "../models/index.js"
 
-const getAllMembers = async () =>{
-    let members = await Member.find()
-    return members
+const getAllProducts = async () =>{
+    let Products = await Product.find()
+    return Products
 }
+
+const getProductById = async (productId) =>{
+    let Product = await Product.findById(productId)
+    if(!Product) {
+        throw new Exception("Cannot find Product with id"+productId)
+    }
+    return Product
+}
+
+const insertProduct = async ({name, quantity, price}) =>{
+    quantity = parseInt(quantity)
+    price = parseFloat(price)
+   try{
+        const product = await Product.create({
+            name,
+            quantity,
+            price
+        })
+        return product
+   }catch(exception){
+    if(!!exception.errors) {
+        throw new Exception("Input error")
+    }
+   }
+    
+}
+
 export default {
-    getAllMembers
+    getAllProducts,
+    getProductById,
+    insertProduct
    
 }
